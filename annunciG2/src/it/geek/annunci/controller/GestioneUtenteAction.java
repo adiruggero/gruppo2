7ddea40287367b519e83b3861f19453ebf8f55d4
Antonio Scarpati <antoscarpati@gmail.com>
2013-05-21 10:15:33 +0200

    aggiornamento

--- annunciG2/src/it/geek/annunci/controller/GestioneUtenteAction.java
+++ annunciG2/src/it/geek/annunci/controller/GestioneUtenteAction.java
@@ -1,15 +1,18 @@
 package it.geek.annunci.controller;
 
 import it.geek.annunci.form.UtentiForm;
+import it.geek.annunci.model.Utente;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
+import org.omg.CORBA.Request;
 
 public class GestioneUtenteAction extends DispatchAction{
 
@@ -22,6 +25,16 @@ public class GestioneUtenteAction extends DispatchAction{
 			throws Exception{
 		
 		UtentiForm utenti = (UtentiForm) form;
+		HttpSession session = request.getSession();
+		
+		Utente u = (Utente) session.getAttribute("utenteSessione");
+		
+		if(u==null){
+			//Login
+		} else{
+			//
+		}
+		
 		
 		
 		
